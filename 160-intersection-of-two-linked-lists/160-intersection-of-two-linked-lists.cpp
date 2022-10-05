110d7b658c2276aabaf72ecef1431d76a848a3c6
superhoan <hoan704@gmail.com>
2022-10-05 22:49:49 +0800

    Time: 100 ms (38.74%), Space: 14.6 MB (32.60%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -11,20 +11,21 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode *ptr1 = headA;
 		ListNode *ptr2 = headB;
-		while(ptr1 != ptr2){
-			if(ptr1 == NULL){
-				ptr1 = headB;
-			}
-			else{
-				ptr1 = ptr1 -> next;
-			}
-			if(ptr2 == NULL){
-				ptr2 = headA;
-			}
-			else{
-				ptr2 = ptr2 -> next;
-			}
-		}
+        while(ptr1!=ptr2){
+            if(ptr1==nullptr && ptr2==nullptr){
+                return nullptr;
+            }
+            if(ptr1==nullptr){
+                ptr1 = headB;
+            }else{
+                ptr1 = ptr1->next;
+            }
+            if(ptr2==nullptr){
+                ptr2 = headA;
+            }else{
+                ptr2 = ptr2->next;
+            }
+        }
 		return ptr1;
     }
 };
