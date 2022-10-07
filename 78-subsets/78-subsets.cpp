99a1d716b8cdc652f0758a39db1797e7ffbc0f12
superhoan <hoan704@gmail.com>
2022-10-07 23:13:19 +0800

    Time: 3 ms (76.54%), Space: 7.2 MB (70.44%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,19 +1,41 @@
+// class Solution {
+// public:
+//     void backtrack(const vector<int>& nums, int curr, vector<int> sub){
+//         if(curr==nums.size()){
+//             res.push_back(sub);
+//             return;
+//         } 
+//         backtrack(nums, curr+1, sub);
+//         sub.push_back(nums[curr]);
+//         backtrack(nums, curr+1, sub);
+//     }
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<int> sub;
+//         backtrack(nums,0,sub);
+//         return res;
+//     }
+// private:
+//     vector<vector<int>> res;
+// };
 class Solution {
 public:
-    void backtrack(const vector<int>& nums, int curr, vector<int> sub){
-        if(curr==nums.size()){
-            res.push_back(sub);
-            return;
-        } 
-        backtrack(nums, curr+1, sub);
-        sub.push_back(nums[curr]);
-        backtrack(nums, curr+1, sub);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> sub;
-        backtrack(nums,0,sub);
-        return res;
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int num = pow(2, n), i = 0;
+        
+        while(i<num)
+        {
+            vector<int> temp;
+            for(int j=0; j<n; j++)
+            {
+                if(i & 1<<j)
+                    temp.push_back(nums[j]);
+            }
+            ans.push_back(temp);
+            i++;
+        }
+        
+        return ans;
     }
-private:
-    vector<vector<int>> res;
 };
