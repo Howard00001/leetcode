10f316af9e34c4da5439e99f7736000b0a45ecb0
superhoan <hoan704@gmail.com>
2022-10-13 18:09:50 +0800

    Time: 136 ms (81.81%), Space: 67.7 MB (90.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,26 +1,17 @@
-int init = [] {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    ofstream out("user.out");
-    cout.rdbuf(out.rdbuf());
-    for (string s; getline(cin, s); cout << '\n') {
-        int ans = INT_MIN, sum = 0;
-        for (int _i = 1, _n = s.length(); _i < _n; ++_i) {
-            bool _neg = false;
-            if (s[_i] == '-') ++_i, _neg = true;
-            int v = s[_i++] & 15;
-            while ((s[_i] & 15) < 10) v = v * 10 + (s[_i++] & 15);
-            if (_neg) v = -v;
-            sum = max(sum + v, v);
-            ans = max(ans, sum);
-        }
-        cout << ans;
-    }
-    exit(0);
-    return 0;
-}();
-
 class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {return 0;}
+public:      
+    int maxSubArray(vector<int>& nums) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);        
+        int sum = 0, smax = INT_MIN;
+        for (int num : nums) {
+            sum += num;
+            smax = max(smax, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return smax;
+    }
 };
