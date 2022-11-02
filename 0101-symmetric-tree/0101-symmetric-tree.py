1ef81ec47ef39dc26a256e409de486f36787e507
superhoan <hoan704@gmail.com>
2022-11-02 15:55:14 +0800

    Time: 52 ms (75.17%), Space: 14 MB (61.05%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -4,12 +4,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def dfs2(root, root2):
-    if(not root and not root2):
-        return True
-    if(root and root2 and root.val==root2.val):
-        return dfs2(root.left, root2.right) and dfs2(root.right, root2.left)
-    return False
+# def dfs2(root, root2):
+#     if(not root and not root2):
+#         return True
+#     if(root and root2 and root.val==root2.val):
+#         return dfs2(root.left, root2.right) and dfs2(root.right, root2.left)
+#     return False
+# class Solution:
+#     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+#         return dfs2(root.left,root.right)
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return dfs2(root.left,root.right)
+        que = []
+        que.append(root.left)
+        que.append(root.right)
+        while(que):
+            left=que[0]
+            que.pop(0)
+            right=que[0]
+            que.pop(0)
+            
+            if (not left and right) or (left and not right):
+                return False
+            if left:
+                if left.val!=right.val:
+                    return False
+                que.append(left.left)
+                que.append(right.right)
+                que.append(left.right)
+                que.append(right.left)
+        return True
