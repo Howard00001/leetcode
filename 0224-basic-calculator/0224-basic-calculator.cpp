a914a23c0ebf9d4a4661a3abf715f8b27ff749e5
superhoan <hoan704@gmail.com>
2022-11-21 11:08:54 +0800

    Time: 3 ms (99.42%), Space: 7.9 MB (93.02%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,83 +1,34 @@
 class Solution {
 public:
-    // int calculate(string s) {
-    //     stack<pair<int,int>> st;
-    //     int num=0;
-    //     int sign=1;
-    //     for(int i=0;i<s.size();i++){
-    //         if(s[i]==' ') continue;
-    //         if(isdigit(s[i])){
-    //             int i0=s[i]-'0';
-    //             while(i+1<s.size() && isdigit(s[i+1])) {
-    //                 i0=i0*10+s[i+1]-'0';
-    //                 i++;
-    //             }
-    //             num += i0*sign;
-    //             sign = 1;
-    //         }
-    //         if(s[i]=='-') sign = -1;
-    //         if(s[i]=='+') sign = 1;
-    //         if(s[i]=='('){
-    //             st.push({num, sign});
-    //             num = 0;
-    //             sign = 1;
-    //         }
-    //         if(s[i]==')'){
-    //             int a = st.top().first;
-    //             sign = st.top().second;
-    //             st.pop();
-    //             num = a+num*sign;
-    //         }
-    //     }
-    //     return num;
-    // }
     int calculate(string s) {
-        int size = s.size();
-
-        int i = 0, ans = 0;
-
-        char prevOperation = '+';
-        stack<pair<int, char>> st;
-        while(i < size){
-            // If string having space
-            if(s[i] == ' '){
-                i++;
-                continue;
+        stack<pair<int,int>> st;
+        int num=0;
+        int sign=1;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' ') continue;
+            if(isdigit(s[i])){
+                int i0=s[i]-'0';
+                while(i+1<s.size() && isdigit(s[i+1])) {
+                    i0=i0*10+(s[i+1]-'0');
+                    i++;
+                }
+                num += i0*sign;
+                sign = 1;
             }
-
-            // Check if string having digit
-            int num = 0;
-            while(i < size && isdigit(s[i])){
-                num = 10 * num + (s[i] - '0');
-                i++;
-            } 
-
-            // Which operator is consist by string
-            if(prevOperation == '+')
-                ans += num;
-            else if(prevOperation == '-')
-                ans += -1 * num;
-
-            // Solve the ()
-            if(s[i] == '('){
-                st.push({ans, prevOperation});
-                ans = 0;
-                prevOperation = '+';
-                i++;
-                continue;
+            if(s[i]=='-') sign = -1;
+            if(s[i]=='+') sign = 1;
+            if(s[i]=='('){
+                st.push({num, sign});
+                num = 0;
+                sign = 1;
             }
-            else if(s[i] == ')'){
-                auto top = st.top();
+            if(s[i]==')'){
+                int a = st.top().first;
+                sign = st.top().second;
                 st.pop();
-                if(top.second == '-')
-                    ans = -1 * ans + top.first;
-                else
-                    ans = ans + top.first;
+                num = a+num*sign;
             }
-
-            prevOperation = s[i];
-            i++;          
         }
-        return ans;
+        return num;
     }
 };
