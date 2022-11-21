bf9b77f1c9c171393ce751e46bac7ff67efd513e
superhoan <hoan704@gmail.com>
2022-11-22 00:36:25 +0800

    Time: 162 ms (78.80%), Space: 34.4 MB (63.57%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,100 +1,77 @@
-class DisjointSet {
-    vector<int> rank, parent, size; 
-public: 
-    DisjointSet(int n) {
-        rank.resize(n+1, 0); 
-        parent.resize(n+1);
-        size.resize(n+1); 
-        for(int i = 0;i<=n;i++) {
+class DSU {
+    vector<int> rank, parent, size;
+public:
+    DSU(int n){
+        rank.resize(n,0);
+        parent.resize(n);
+        size.resize(n,1);
+        for(int i = 0;i<n;i++) {
             parent[i] = i; 
-            size[i] = 1; 
+            //size[i] = 1; 
+        }
+        //iota(parent.begin(),parent.end(),0);
+    }
+    int find(int x){
+        if(x==parent[x]) return x;
+        return parent[x]=find(parent[x]);
+    }
+    void unionByRank(int x, int y){
+        x = find(x);
+        y = find(y);
+        if(x==y) return;
+        if(rank[x]<rank[y]) parent[x] = y;
+        else if(rank[y]<rank[x]) parent[y] = x;
+        else{
+            parent[y]=x;
+            rank[x]++;
         }
     }
-
-    int findUPar(int node) {
-        if(node == parent[node])
-            return node; 
-        return parent[node] = findUPar(parent[node]); 
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u); 
-        int ulp_v = findUPar(v); 
-        if(ulp_u == ulp_v) return; 
-        if(rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v; 
-        }
-        else if(rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u; 
-        }
-        else {
-            parent[ulp_v] = ulp_u; 
-            rank[ulp_u]++; 
+    void unionBySize(int x, int y){
+        x = find(x);
+        y = find(y);
+        if(size[x] < size[y]){
+            parent[x] = y;
+            size[y] += size[x];
+        } 
+        else{
+            parent[y] = x;
+            size[x] += size[y];
         }
     }
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u); 
-        int ulp_v = findUPar(v); 
-        if(ulp_u == ulp_v) return; 
-        if(size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v; 
-            size[ulp_v] += size[ulp_u]; 
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v]; 
-        }
-    }
-}; 
+};
+////////////////////////////////////////////////////////////////////////////////////
 class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts)
-    {
-        int n=accounts.size();
-        DisjointSet ds(n);
-        unordered_map<string,int>mp;
-        // step 1
-        for(int i=0;i<n;i++)
-        {
-         for(int j=1;j<accounts[i].size();j++)
-         {
-             string s=accounts[i][j];
-             if(mp.find(s)!=mp.end())
-             {
-                 ds.unionByRank(i,mp[s]);
-             }
-             else
-             {
-                 mp[s]=i;
-             }
-         }
-        }
-        // step 2
-        vector<string>merge[n];
-        for(auto it:mp)
-        {
-            string node=it.first;
-            int kk=ds.findUPar(it.second);
-            merge[kk].push_back(node);
-        }
-        // step 3
-        vector<vector<string>>ans;
-        for(int i=0;i<n;i++)
-        {
-            if(merge[i].size()==0)
-            {
-                continue;
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        DSU dsu(accounts.size());
+        unordered_map<string, int> m;
+        // 1
+        for(int i=0;i<accounts.size();i++){
+            for(int j=1;j<accounts[i].size();j++){
+                string s = accounts[i][j];
+                if(m.count(s)) dsu.unionByRank(i, m[s]);
+                else m[s] = i;
             }
-            sort(merge[i].begin(),merge[i].end());
-            vector<string>temp;
-            temp.push_back(accounts[i][0]);
-            for(auto it:merge[i])
-            {
-              temp.push_back(it);
-            }
-            ans.push_back(temp);
         }
-        return ans;
+        // 2
+        vector<vector<string>> sets(accounts.size());
+        for(auto it:m){
+            string mail=it.first;
+            int root = dsu.find(it.second);
+            sets[root].push_back(mail);
+        }
+        // 3
+        vector<vector<string>> res;
+        for(int i=0;i<accounts.size();i++){
+            if(sets[i].size()==0) continue;
+            sort(sets[i].begin(),sets[i].end());
+            vector<string> tmp;
+            tmp.push_back(accounts[i][0]);
+            for(auto it:sets[i]){
+                tmp.push_back(it);
+            }
+            res.push_back(tmp);
+        }
+        return res;
     }
 };
