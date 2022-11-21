52c5f4beaf6206ddd295aaa51f57b21a2088c2f4
superhoan <hoan704@gmail.com>
2022-11-21 20:49:00 +0800

    Time: 169 ms (76.51%), Space: 34.3 MB (65.31%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,54 +1,100 @@
+class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
 class Solution {
 public:
-    unordered_map<string, string> parent;
-    unordered_map<string, string> owner;
-    unordered_map<string, set<string>> group;
-    
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        // 1. initial
-        for(int i=0;i<accounts.size();i++){
-            for(int j=1;j<accounts[i].size();j++){
-                parent[accounts[i][j]] = accounts[i][j];
-                owner[accounts[i][j]] = accounts[i][0];
-            }
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts)
+    {
+        int n=accounts.size();
+        DisjointSet ds(n);
+        unordered_map<string,int>mp;
+        // step 1
+        for(int i=0;i<n;i++)
+        {
+         for(int j=1;j<accounts[i].size();j++)
+         {
+             string s=accounts[i][j];
+             if(mp.find(s)!=mp.end())
+             {
+                 ds.unionByRank(i,mp[s]);
+             }
+             else
+             {
+                 mp[s]=i;
+             }
+         }
         }
-        // 2. Union
-        for(int i=0;i<accounts.size();i++){
-            for(int j=2;j<accounts[i].size();j++){
-                if(FindSet(accounts[i][j])!=FindSet(accounts[i][j-1]))
-                    Union(accounts[i][j], accounts[i][j-1]);
-            }
+        // step 2
+        vector<string>merge[n];
+        for(auto it:mp)
+        {
+            string node=it.first;
+            int kk=ds.findUPar(it.second);
+            merge[kk].push_back(node);
         }
-        // 3. Create Set
-        for(int i=0;i<accounts.size();i++){
-            for(int j=1;j<accounts[i].size();j++){
-                string p = FindSet(accounts[i][j]);
-                group[p].insert(accounts[i][j]);
+        // step 3
+        vector<vector<string>>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(merge[i].size()==0)
+            {
+                continue;
             }
-        }
-        // 4. Generate result
-        vector<vector<string>> res;
-        for(auto g:group){
-            vector<string> tmp;
-            tmp.push_back(owner[g.first]);
-            for(auto &s:g.second){
-                tmp.push_back(s);
+            sort(merge[i].begin(),merge[i].end());
+            vector<string>temp;
+            temp.push_back(accounts[i][0]);
+            for(auto it:merge[i])
+            {
+              temp.push_back(it);
             }
-            res.push_back(tmp);
+            ans.push_back(temp);
         }
-        return res;
-    }
-    
-    string FindSet(string x){
-        if(x!=parent[x])
-            parent[x] = FindSet(parent[x]);
-        return parent[x];
-    }
-    
-    void Union(string x, string y){
-        x = parent[x];
-        y = parent[y];
-        if(x>y) parent[x] = y;
-        else parent[y] = x;
+        return ans;
     }
 };
