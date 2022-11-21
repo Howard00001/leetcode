ef8e7d978771590620346df741c096519ff502de
superhoan <hoan704@gmail.com>
2022-11-21 22:04:21 +0800

    Time: 196 ms (63.53%), Space: 34.2 MB (70.75%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,50 +1,100 @@
-class Solution {
-private:
-    unordered_map<string, vector<string>> graph;
-    unordered_set<string> visited;
-    unordered_map<string,string> email_to_name;
+class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
 
-    void buildGraph(vector<vector<string>>& accounts) {
-        for(int i = 0; i < accounts.size(); ++i) {
-            string name = accounts[i][0];
-            string first_email = accounts[i][1];
-            
-            for(int j = 1; j < accounts[i].size(); ++j) {
-                string email = accounts[i][j];
-                graph[first_email].push_back(email);
-                graph[email].push_back(first_email);
-                if(!email_to_name.count(email) ) {
-                    email_to_name[email] = name;
-                }
-            }
-        }
-        
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
     }
-    void dfs(string email, set<string> &email_set) {
-        if(visited.count(email)) {
-            return;
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
         }
-        visited.insert(email);
-        email_set.insert(email);
-        for(auto email : graph[email]) {
-            dfs(email, email_set);
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
         }
     }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
+class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        vector<vector<string>> res;
-        buildGraph(accounts);
-        for(auto [email, _] : graph) {
-            if(!visited.count(email)) {
-                set<string> email_set;
-                dfs(email, email_set);
-                vector<string> account(email_set.size() + 1);
-                string name = email_to_name[email];
-                account = {email_set.begin(), email_set.end()};
-                account.insert(account.begin(), name);
-                res.push_back(account);
-            }
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts)
+    {
+        int n=accounts.size();
+        DisjointSet ds(n);
+        unordered_map<string,int>mp;
+        // step 1
+        for(int i=0;i<n;i++)
+        {
+         for(int j=1;j<accounts[i].size();j++)
+         {
+             string s=accounts[i][j];
+             if(mp.find(s)!=mp.end())
+             {
+                 ds.unionByRank(i,mp[s]);
+             }
+             else
+             {
+                 mp[s]=i;
+             }
+         }
         }
-        return res;
+        // step 2
+        vector<string>merge[n];
+        for(auto it:mp)
+        {
+            string node=it.first;
+            int kk=ds.findUPar(it.second);
+            merge[kk].push_back(node);
+        }
+        // step 3
+        vector<vector<string>>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(merge[i].size()==0)
+            {
+                continue;
+            }
+            sort(merge[i].begin(),merge[i].end());
+            vector<string>temp;
+            temp.push_back(accounts[i][0]);
+            for(auto it:merge[i])
+            {
+              temp.push_back(it);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
     }
 };
