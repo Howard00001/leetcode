81b193aa99963d4bcf69e50e86d833286899596b
superhoan <hoan704@gmail.com>
2022-11-22 10:40:13 +0800

    Time: 33 ms (36.98%), Space: 13.2 MB (42.70%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -6,7 +6,7 @@ public:
         dp[0]=true;
         for(int i=1;i<=s.size();i++){
             for(int j=1;j<=i;j++){
-                if(!dp[j-1]) continue;
+                if(!dp[j-1] || j-i>=20) continue;
                 string sub = s.substr(j-1,i-j+1);
                 if(wordSet.count(sub)){
                     dp[i] = true;
