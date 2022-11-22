fcf70e327d610cdb618834f81dacf7caad7b25c9
superhoan <hoan704@gmail.com>
2022-11-22 10:21:26 +0800

    Time: 41 ms (27.41%), Space: 13.2 MB (42.70%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size()+1,false);
+        unordered_set<string> wordSet(wordDict.begin(),wordDict.end());
+        dp[0]=true;
+        for(int i=1;i<=s.size();i++){
+            for(int j=1;j<=i;j++){
+                if(!dp[j-1]) continue;
+                string sub = s.substr(j-1,i-j+1);
+                if(wordSet.count(sub)){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
