383d41ea886130cf39de1223636f89f936a2f7d4
superhoan <hoan704@gmail.com>
2022-11-23 11:43:14 +0800

    Time: 1883 ms (63.52%), Space: 14 MB (84.75%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,7 +4,19 @@ class Solution:
         if (s&1)==1:
             return False
         s = int(s/2)
-        bits = 1
+        dp = [False]*(s+1)
+        dp[0] = True
         for num in nums:
-            bits = bits | (bits<<num)
-        return bits&pow(2,s)
+            for i in range(s,-1,-1):
+                if i-num >=0:
+                    dp[i] = dp[i] | dp[i-num]
+        return dp[s]
+    
+        # s = sum(nums)
+        # if (s&1)==1:
+        #     return False
+        # s = int(s/2)
+        # bits = 1
+        # for num in nums:
+        #     bits = bits | (bits<<num)
+        # return bits&pow(2,s)
