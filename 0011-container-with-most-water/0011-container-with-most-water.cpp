4db73721f709b693631e6ba84ffc011792610bdf
superhoan <hoan704@gmail.com>
2022-11-24 23:47:13 +0800

    Time: 271 ms (25.47%), Space: 59.1 MB (36.88%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,7 +4,8 @@ public:
         int maxA = 0;
         int L=0,R=height.size()-1;
         while(L<R){
-            maxA = max(min(height[L],height[R])*(R-L), maxA);
+            int curr = min(height[L],height[R])*(R-L);
+            if(curr>maxA) maxA= curr;
             if(height[L]<height[R]) L++;
             else R--;
         }
