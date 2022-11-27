ac270ae2c81c4bf8f0f987dac363c07d2d70d418
superhoan <hoan704@gmail.com>
2022-11-28 00:20:07 +0800

    Time: 313 ms (82.97%), Space: 8.1 MB (46.31%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int dir[5] = {-1,0,1,0,-1};
-    bool backtrack(vector<vector<char>>& board, string word, int i, int j, int curr){
+    bool backtrack(vector<vector<char>>& board, const string& word, int i, int j, int curr){
         if(curr == word.size()) return true;
         if(i<0||j<0||i==board.size()||j==board[0].size() || board[i][j]!=word[curr]) return false;
-        char tmp = board[i][j];
         board[i][j]='0';
         for(int d=0;d<4;d++){
             if(backtrack(board, word, i+dir[d], j+dir[d+1], curr+1)){
-                board[i][j]=tmp;
+                board[i][j]=word[curr];
                 return true;
             } 
         }
-        board[i][j]=tmp;
+        board[i][j]=word[curr];
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
