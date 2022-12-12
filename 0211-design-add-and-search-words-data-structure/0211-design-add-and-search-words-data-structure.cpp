116566b5e1088c1e79a864479f6e8f232fb2d1af
superhoan <hoan704@gmail.com>
2022-12-12 13:11:36 +0800

    Time: 2823 ms (8.27%), Space: 623.6 MB (11.15%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -1,31 +1,83 @@
+// class WordDictionary {
+// public:
+// 	vector<vector<string>> dict;
+// 	unordered_set<string> rec;
+// 	WordDictionary() {
+// 		dict = vector<vector<string>>(25);
+// 	}
+// 	void addWord(string word) {
+// 		if (rec.find(word) == rec.end()) {
+// 			dict[word.length() - 1].push_back(word);
+// 			rec.insert(word);
+// 		}
+// 	}
+// 	bool search(string word) {
+// 		for (int i = 0; i < word.length(); ++i)
+// 			if (word[i] == '.') {
+// 				for (int j = 0; j < dict[word.length() - 1].size(); ++j) {
+//                     int k;
+// 					for (k = 0; k < word.length(); ++k)
+// 						if (word[k] != dict[word.length() - 1][j][k] && word[k] != '.')
+// 							break;
+// 					if (k == word.length())
+// 						return true;
+// 				}
+// 				return false;
+// 			}
+// 		return rec.find(word) != rec.end();
+// 	}
+// };
+
+class Node {
+public:
+    vector<Node*> next;
+    bool isend;
+    
+    Node(){
+        next = vector<Node*> (26,nullptr);
+        isend = false;
+    }
+    
+};
+
 class WordDictionary {
-  public:
-	vector<vector<string>> dict;
-	unordered_set<string> rec;
-	WordDictionary() {
-		dict = vector<vector<string>>(25);
-	}
-	void addWord(string word) {
-		if (rec.find(word) == rec.end()) {
-			dict[word.length() - 1].push_back(word);
-			rec.insert(word);
-		}
-	}
-	bool search(string word) {
-		int length = word.length();
-		for (int i = 0; i < length; ++i)
-			if (word[i] == '.') {
-				for (int j = 0, k; j < dict[length - 1].size(); ++j) {
-					for (k = 0; k < word.length(); ++k)
-						if (word[k] != dict[length - 1][j][k] && word[k] != '.')
-							break;
-					if (k == length)
-						return true;
-				}
-				return false;
-			}
-		return rec.find(word) != rec.end();
-	}
+public:
+    //unordered_map<string, bool> buf;
+    Node *root;
+    
+    WordDictionary() {
+        root = new Node();
+    }
+    
+    void addWord(string word) {
+        Node* curr = root;
+        for(auto c:word){
+            if(curr->next[c-'a']==nullptr)
+                curr->next[c-'a'] = new Node();
+            curr = curr->next[c-'a'];
+        }
+        curr->isend = true;
+    }
+    
+    bool search(string word) {
+        return searchTrie(root, word, 0);
+    }
+    
+    bool searchTrie(Node* curr, string &word, int st){
+        for(int i=st;i<word.length();i++){
+            char c = word[i];
+            if(c=='.'){
+                for(auto p:curr->next){
+                    if(p && searchTrie(p, word, i+1))
+                        return true;
+                }
+                return false;
+            }
+            if(curr->next[c-'a']==nullptr) return false;
+            curr = curr->next[c-'a'];
+        }
+        return curr && curr->isend;
+    }
 };
 /**
  * Your WordDictionary object will be instantiated and called as such:
