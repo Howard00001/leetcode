3e0cfb9b38ee1271a315e6a439e1817320db6968
superhoan <hoan704@gmail.com>
2023-03-01 20:33:21 +0800

    Time: 124 ms (97.28%), Space: 15.1 MB (68.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,3 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        return int((1+len(nums))*len(nums)/2) - sum(nums)
+        #return int((1+len(nums))*len(nums)/2) - sum(nums)
+    
+        res = n = len(nums)
+        for i in range(n):
+            res ^= nums[i]^i
+        return res
