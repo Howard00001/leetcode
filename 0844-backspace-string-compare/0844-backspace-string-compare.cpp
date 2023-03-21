cae92425830710e92e4fcb9e70236183152fc8c3
superhoan <hoan704@gmail.com>
2023-03-21 19:51:56 +0800

    Time: 2 ms (51.60%), Space: 6.4 MB (50.26%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int n1=s.size()-1, n2=t.size()-1, back;
-        while(true){
-            back=0;
-            while(n1>=0 && (back||s[n1]=='#')){
-                back += s[n1]=='#'?1:-1;
-                n1--;
-            }
-            back=0;
-            while(n2>=0 && (back||t[n2]=='#')){
-                back += t[n2]=='#'?1:-1;
-                n2--;
-            }
-            if(n1>=0 && n2>=0 && s[n1]==t[n2]){
-                n1--;
-                n2--;
+        string sn="", tn="";
+        for(auto c:s){
+            if(c=='#'){
+                if(!sn.empty())
+                    sn.pop_back();
             }
             else
-                break;
+                sn.push_back(c);
         }
-        return n1==-1 && n2==-1;
+        for(auto c:t){
+            if(c=='#'){
+                if(!tn.empty())
+                    tn.pop_back();
+            }
+            else
+                tn.push_back(c);
+        }
+        return sn==tn;
     }
 };
