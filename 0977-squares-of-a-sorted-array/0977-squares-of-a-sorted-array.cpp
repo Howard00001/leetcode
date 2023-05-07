db7bee6c1995d15e2209169c3a44ea62e980dc05
superhoan <hoan704@gmail.com>
2023-05-07 20:41:01 +0800

    Time: 35 ms (71.08%), Space: 26 MB (33.65%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -5,12 +5,10 @@ public:
         int left=0, right=nums.size()-1, curr=right;
         for(;curr>=0;curr--){
             if(abs(nums[left])<abs(nums[right])){
-                res[curr] = nums[right]*nums[right];
-                right--;
+                res[curr] = nums[right]*nums[right--];
             }
             else{
-                res[curr] = nums[left]*nums[left];
-                left++;
+                res[curr] = nums[left]*nums[left++];
             }
         }
         return res;
